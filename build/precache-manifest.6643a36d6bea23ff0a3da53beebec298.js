5b0af2f24314f74728d8a9fbc50142397c3b0e7a
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-11-28 13:40:18 -0500

    New dist folder

---
+++ build/precache-manifest.6643a36d6bea23ff0a3da53beebec298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8fe601e9df90b5871c78e548f7f504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab13711812ac1e30076",
+    "url": "/static/css/main.78fee0e0.chunk.css"
+  },
+  {
+    "revision": "709a4f481df4b6809a1d",
+    "url": "/static/js/2.45ea96e7.chunk.js"
+  },
+  {
+    "revision": "653ac1c43bf30a302d9476ac1d639668",
+    "url": "/static/js/2.45ea96e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab13711812ac1e30076",
+    "url": "/static/js/main.ed8bf94e.chunk.js"
+  },
+  {
+    "revision": "1fde3f2ab36ae23d0dc9",
+    "url": "/static/js/runtime-main.92a273ff.js"
+  }
+]);
